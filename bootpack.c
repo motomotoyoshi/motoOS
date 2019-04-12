6907cb2ad18cd2c0318f8c9e843b6d6aa7e01f57
motomotoyoshi <motomoto.git@gmail.com>
2019-04-12 15:55:39 +0000

    display squre

--- bootpack.c
+++ bootpack.c
@@ -4,24 +4,38 @@ void io_out8(int port, int data);
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
 
-/* 同じソースファイル内でも定義する前に使う場合宣言が必要 */
-
 void init_palette(void);
-void set_palett(int start, int end, unsigned char *rgb)
+void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+
+#define COL8_000000   0
+#define COL8_FF0000   1
+#define COL8_00FF00   2
+#define COL8_FFFF00   3
+#define COL8_0000FF   4
+#define COL8_FF00FF   5
+#define COL8_00FFFF   6
+#define COL8_FFFFFF   7
+#define COL8_C6C6C6   8
+#define COL8_840000   9
+#define COL8_008400   10
+#define COL8_848400   11
+#define COL8_000084   12
+#define COL8_840084   13
+#define COL8_008484   14
+#define COL8_848484   15
 
 void HariMain(void)
 {
-  int i; /* 変数宣言 iという変数は32ビットの整数型} */
-
   char *p; /* pという変数はBYTE[...]用の番地 */
 
   init_palette();
 
   p = (char *) 0xa0000; /* 番地を代入 */
 
-  for(i = 0; i <= 0xffff; i++) {
-    p[i] = i & 0x0f;
-    }
+  boxfill8(p, 320, COL8_FF0000, 20, 20, 120, 120);
+  boxfill8(p, 320, COL8_00FF00, 70, 50, 170, 150);
+  boxfill8(p, 320, COL8_0000FF, 120, 80, 220, 180);
 
   for(;;){
     io_hlt();
@@ -44,17 +58,17 @@ void init_palette(void)
     0x00, 0x84, 0x00, /* 10:暗い緑 */
     0x84, 0x84, 0x00, /* 11:暗い黄色 */
     0x00, 0x00, 0x84, /* 12:暗い青 */
-    0x00, 0x84, 0x84, /* 13:暗い紫 */
+    0x84, 0x00, 0x84, /* 13:暗い紫 */
     0x00, 0x84, 0x84, /* 14:暗い水色 */
     0x84, 0x84, 0x84  /* 15:暗い灰色 */
   };
-  set_palett(0, 15, table_rgb);
+  set_palette(0, 15, table_rgb);
   return;
 
   /* static char 命令は、データにしか使えないけどDB命令相当*/
 }
 
-void set_palett(int start, int end, unsigned char *rgb)
+void set_palette(int start, int end, unsigned char *rgb)
 {
   int i, eflags;
   eflags = io_load_eflags(); /* 割り込み許可フラグの値を記録する */
@@ -67,5 +81,15 @@ void set_palett(int start, int end, unsigned char *rgb)
     rgb += 3;
     }
   io_store_eflags(eflags); /* 割り込み許可フラグを元に戻す */
+  return;
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+  int x, y;
+  for(y = y0; y <= y1; y++){
+    for(x = x0; x <= x1; x++)
+      vram[y * xsize + x] = c;
+  }
   return;
 }
--- nasmfunc.asm
+++ nasmfunc.asm
@@ -3,14 +3,72 @@
 
 BITS 32           ; 32ビットモード用の機械語を作らせる
 
-GLOBAL  io_hlt, write_mem8 ; このプログラムに含まれる関数名
-SECTION .text     ; オブジェクトファイルではこれを書いてからプログラムを書く
+GLOBAL  io_hlt, io_cli, io_sti, io_stihlt 
+GLOBAL  io_in8, io_in16, io_in32
+GLOBAL  io_out8, io_out16, io_out32
+GLOBAL  io_load_eflags, io_store_eflags
+
+SECTION .text 
+
 io_hlt:   ; void io_hlt(void);
   HLT
   RET
 
-write_mem8: ; void write_mem8(int addr, int date);
-  MOV   ECX,[ESP+4] ; [ESP+4]にaddrが入っているのでそれをECXに読み込む
-  MOV   AL,[ESP+8] ;  [ESP+8]にdataが入っているのでそれをALに読み込む
-  MOV   [ECX],AL
+io_cli:   ; void io_cli(void);
+  CLI
+  RET
+
+io_sti:   ; void io_sti(void);
+  STI
+  RET
+
+io_stihlt: ; void io_stihlt(void);
+  STI
+  HLT
+  RET
+
+io_in8:    ; void io_in8(int port);
+  MOV   EDX,[ESP+4] ; port
+  MOV   EAX,0
+  IN    AL,DX
+  RET
+
+io_in16:    ; void io_in16(int port);
+  MOV   EDX,[ESP+4] ; port
+  MOV   EAX,0
+  IN    AX,DX
+  RET
+
+io_in32:    ; void io_in32(int port);
+  MOV   EDX,[ESP+4] ; port
+  IN    EAX,DX
+  RET
+
+io_out8:   ; void io_out8(int port, int data);
+  MOV   EDX,[ESP+4] ; port
+  MOV   AL,[ESP+8]  ; data
+  OUT   DX,AL
+  RET
+
+io_out16:   ; void io_out16(int port, int data);
+  MOV   EDX,[ESP+4] ; port
+  MOV   EAX,[ESP+8]  ; data
+  OUT   DX,AX
+  RET
+
+io_out32:   ; void io_out32(int port, int data);
+  MOV   EDX,[ESP+4] ; port
+  MOV   EAX,[ESP+8]  ; data
+  OUT   DX,EAX
+  RET
+
+io_load_eflags:  ; int io_load_eflags(void);
+  PUSHFD    ; PUSH EFLAGS という意味
+  POP   EAX
+  RET
+
+io_store_eflags:  ; void io_store_eflags(int eflags);
+  MOV   EAX,[ESP+4]
+  PUSH  EAX
+  POPFD   ; POP EFLAGS という意味
   RET
