af8a37e7d7096fa4359a72b1763aac0e4fd9defc
motomotoyoshi <motomoto.git@gmail.com>
2019-05-12 17:54:57 +0000

    update int.c etc

--- bootpack.c
+++ bootpack.c
@@ -5,12 +5,13 @@ extern void mysprintf(char *str, char *fmt, ...);
 
 void HariMain(void)
 {
-  struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
+  struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
   char s[40], mcursor[256];
   int mx, my;
 
   init_gdtidt();
   init_pic();
+  io_sti();
 
   init_palette();
   init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -20,6 +21,9 @@ void HariMain(void)
   putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
   mysprintf(s, "(%d, %d)", mx, my);
   putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
+
+  io_out8(PIC0_IMR, 0xf9);
+  io_out8(PIC1_IMR, 0xef);
 
   for(;;){
     io_hlt();
