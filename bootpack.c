4eba3c68281a6a907d5df5065fbca4cbb97f60fd
motomotoyoshi <motomoto.git@gmail.com>
2019-05-18 16:55:28 +0000

    update bootpack.c

--- bootpack.c
+++ bootpack.c
@@ -1,14 +1,15 @@
-#include "bootpack.h"
-#include <stdio.h>
-
+#include "bootpack.h" #include <stdio.h>
 extern void mysprintf(char *str, char *fmt, ...);
+
 extern struct FIFO8 keyfifo;
+void enable_mouse(void);
+void init_keyboard(void);
 
 void HariMain(void)
 {
   struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
   char s[40], mcursor[256], keybuf[32];
-  int mx, my, i, j;
+  int mx, my, i;
 
   init_gdtidt();
   init_pic();
@@ -17,6 +18,8 @@ void HariMain(void)
   fifo8_init(&keyfifo, 32, keybuf);
   io_out8(PIC0_IMR, 0xf9);
   io_out8(PIC1_IMR, 0xef);
+
+  init_keyboard();
 
   init_palette();
   init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -27,9 +30,11 @@ void HariMain(void)
   mysprintf(s, "(%d, %d)", mx, my);
   putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
+  enable_mouse();
+
   for(;;){
     io_cli();
-    if (fifo8_status(&keyfifo) == 0){
+    if(fifo8_status(&keyfifo) == 0){
       io_stihlt();
     } else {
       i = fifo8_get(&keyfifo);
@@ -39,4 +44,39 @@ void HariMain(void)
       putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
     }
   }
+}
+
+#define PORT_KEYDAT   0x0060
+#define PORT_KEYSTA   0x0064
+#define PORT_KEYCMD   0x0064
+#define KEYSTA_SEND_NOTREADY   0x02
+#define KEYCMD_WRITE_MODE   0x60
+#define KBC_MODE   0x47
+
+void wait_KBC_sendready(void){
+  for(;;){
+    if((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0){
+      break;
+    }
+  }
+  return;
+}
+
+void init_keyboard(void){
+  wait_KBC_sendready();
+  io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+  wait_KBC_sendready();
+  io_out8(PORT_KEYDAT, KBC_MODE);
+  return;
+}
+
+#define KEYCMD_SENDTO_MOUSE   0xd4
+#define MOUSECMD_ENABLE   0xf4
+
+void enable_mouse(void){
+  wait_KBC_sendready();
+  io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+  wait_KBC_sendready();
+  io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+  return;
 }
